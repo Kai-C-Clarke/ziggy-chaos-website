de069d19575a0db81fd3532f17537fcb2f29d22d
Jon Stiles <j.stiles1066@gmail.com>
2025-11-16 08:02:47 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const fetch = require('node-fetch');
 const app = express();
 const PORT = 3000;
 
+const PORT = process.env.PORT || 3000;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
